e750d1bedfd018b3ba3356f5755cece8111f6eef
Adam Hoang <adamhoang712@gmail.com>
2019-03-27 20:50:57 -0700

    added algos

--- linkedlist.js
+++ linkedlist.js
@@ -13,28 +13,34 @@ class LinkedList {
     insertFirst(data) {
         this.head = new Node(data, this.head);
     }
+    // // using generic function
+    // insertFirst(data) {
+    //     this.insertAt(data, 0);
+    // }
 
     size() {
         let counter = 0;
         let node = this.head;
-
         while (node) {
             counter++;
             node = node.next;
         }
-
         return counter;
     }
 
     getFirst() {
         return this.head;
     }
+    // // using generic function
+    // getFirst() {
+    //     return getAt(0);
+    // }
 
     getLast() {
+        // check to make sure not empty
         if (!this.head) {
             return null;
         }
-
         let node = this.head;
         while (node) {
             if (!node.next) {
@@ -43,29 +49,33 @@ class LinkedList {
             node = node.next;
         }
     }
+    // // using generic function
+    // getLast() {
+    //     return this.getAt(this.size() - 1)
+    // }
 
     clear() {
         this.head = null;
     }
 
     removeFirst() {
+        // check to make sure not empty
         if (!this.head) {
             return;
         }
-
         this.head = this.head.next;
     }
 
     removeLast() {
+        // check to make sure not empty
         if (!this.head) {
             return;
         }
-
+        // check if there is only one node
         if (!this.head.next) {
             this.head = null;
             return;
         }
-
         let previous = this.head;
         let node = this.head.next;
         while (node.next) {
@@ -76,13 +86,13 @@ class LinkedList {
     }
 
     insertLast(data) {
+        // get last node using getLast function
         const last = this.getLast();
-
         if (last) {
-            // There are some existing nodes in our chain
+            // Check if there are nodes
             last.next = new Node(data);
         } else {
-            // The chain is empty!
+            // If there are no nodes
             this.head = new Node(data);
         }
     }
@@ -94,7 +104,6 @@ class LinkedList {
             if (counter === index) {
                 return node;
             }
-
             counter++;
             node = node.next;
         }
@@ -102,53 +111,38 @@ class LinkedList {
     }
 
     removeAt(index) {
+        // check to make sure not empty
         if (!this.head) {
             return;
         }
-
+        // check if number is first node
         if (index === 0) {
             this.head = this.head.next;
             return;
         }
-
+        // find the node before the removeAt index
         const previous = this.getAt(index - 1);
         if (!previous || !previous.next) {
             return;
         }
+        // break the link by pointing to the node after the removeAt index
         previous.next = previous.next.next;
     }
 
     insertAt(data, index) {
+        // check to make sure not empty
         if (!this.head) {
             this.head = new Node(data);
             return;
         }
-
         if (index === 0) {
             this.head = new Node(data, this.head);
             return;
         }
-
+        // get the node before the insertAt index
+        // if there is no node before, the prev is last node
         const previous = this.getAt(index - 1) || this.getLast();
         const node = new Node(data, previous.next);
         previous.next = node;
-    }
-
-    forEach(fn) {
-        let node = this.head;
-        let counter = 0;
-        while (node) {
-            fn(node, counter);
-            node = node.next;
-            counter++;
-        }
-    }
-
-    *[Symbol.iterator]() {
-        let node = this.head;
-        while (node) {
-            yield node;
-            node = node.next;
-        }
     }
 }
